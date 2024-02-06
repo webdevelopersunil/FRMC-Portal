52731bf0f5462a9856bac121866a64eac1890fb3
sunil <he.kingsmen@gmail.com>
2024-02-06 12:02:48 +0530

    Chnages

--- resources/views/test/user/fco/edit.blade.php
+++ resources/views/test/user/fco/edit.blade.php
@@ -41,8 +41,7 @@
               <div class="card">
                 <div class="card-body">
                   <h4 class="card-title">Updation by the office of FCO</h4>
-                  <p class="card-description" onclick="window.location='{{ route('otp.confirmation') }}'" > otp confirmation </p>
-
+                  
                   <form class="forms-sample" action="{{ route('fco.complainant.index') }}" >
                     <div class="row">
                         <div class="col-md-6">
@@ -106,63 +105,49 @@
                     
                     <br> <br>
                     <h5 class="card-title">Updation by the office of FCO</h5>
-                    <div class="row">
-                        <div class="col-md-10">
-                            <div class="form-group">
-                                <label for="exampleInputUsername1">Public</label>
-                                <input type="text" class="form-control">
-                            </div>
-                        </div>
-                      <div class="col-md-2">
-                        <div class="form-group">
-                          <label for="exampleInputUsername1">&nbsp &nbsp</label>
-                            <input type="button" value="Add" class="form-control" >
-                        </div>
-                      </div>
-                    </div>
-
-
-
-            <div class="col-md-12 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">Public – Visible to all users</h4>
-                  <ul>
-                    <li>Lorem ipsum dolor sit amet</li>
-                    <li>Consectetur adipiscing elit</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
 
                     <div class="row">
-                        <div class="col-md-10">
-                            <div class="form-group">
-                                <label for="exampleInputUsername1">Private</label>
-                                <input type="text" class="form-control">
+                      <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="exampleInputUsername1">Public</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control" id="publicInput">
+                                <div class="input-group-append">
+                                    <button type="button" class="btn btn-primary" id="addPublic">Add</button>
+                                </div>
                             </div>
                         </div>
-                      <div class="col-md-2">
-                        <div class="form-group">
-                            <label for="exampleInputUsername1">&nbsp &nbsp</label>
-                            <input type="button" value="Add" class="form-control" >
+                        <div class="card">
+                          <div class="card-body">
+                              <h4 class="card-title">Private – Visible to only the users associated with the office of FCO</h4>
+                              <ul id="private_visible">
+                                  <li>Lorem ipsum dolor sit amet <a href="#" style="color:red; text-align:right;" class="removeItem">X</a></li>
+                              </ul>
+                            </div>
+                          </div>
                         </div>
+
+                      <div class="col-md-6">
+                          <div class="form-group">
+                              <label for="exampleInputUsername1">Private</label>
+                              <div class="input-group">
+                                  <input type="text" class="form-control">
+                                  <div class="input-group-append">
+                                      <button type="button" class="btn btn-primary" id="addPrivate" >Add</button>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="card">
+                            <div class="card-body">
+                              <h4 class="card-title">Public – Visible to all users</h4>
+                              <ul id="private_visible" >
+                                <li>Lorem ipsum dolor sit amet</li>
+                                <li>Consectetur adipiscing elit</li>
+                              </ul>
+                            </div>
+                          </div>
                       </div>
-                    </div>
-
-
-                    <div class="col-md-12 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">Private – Visible to only the users associated with the office of FCO</h4>
-                  <ul>
-                    <li>Lorem ipsum dolor sit amet</li>
-                    <li>Consectetur adipiscing elit</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+                  </div>
 
 
                     <button type="submit" class="btn btn-primary mr-2">Submit</button>
@@ -172,7 +157,7 @@
                 </div>
               </div>
             </div>
-            
+                    
             
             
           </div>
@@ -212,5 +197,28 @@
   <script src="../../js/select2.js"></script>
   <!-- End custom js for this page-->
 </body>
+
+
+<script>
+    document.getElementById("addPublic").addEventListener("click", function() {
+
+        var publicInputValue = document.getElementById("publicInput").value;
+        if(publicInputValue!=''){
+          var privateVisibleList = document.getElementById("private_visible");
+          var newListItem = document.createElement("li");
+          
+          newListItem.innerHTML = publicInputValue + ' <a href="#" style="color:red; text-align:right;" class="removeItem">X</a>';
+          privateVisibleList.appendChild(newListItem); 
+        }
+        publicInputValue.value='';
+    });
+    // Add event listener to dynamically created "X" links
+    document.getElementById("private_visible").addEventListener("click", function(event) {
+        if (event.target.classList.contains("removeItem")) {
+            event.preventDefault(); // Prevent the default behavior of the link
+            event.target.parentElement.remove(); // Remove the list item containing the "X" link
+        }
+    });
+</script>
 
 </html>
